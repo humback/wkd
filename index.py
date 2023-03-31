f4a308559072b169505e9ccf4f8d45a77cc6573a
humback <humback@gmail.com>
2023-03-31 17:26:53 +0200

    vorcel index

--- index.py
+++ index.py
@@ -20,6 +20,6 @@ def hello():
     return "Helllo!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5105,debug=True)
+#if __name__ == '__main__':
+   # app.run(host='0.0.0.0', port=5105,debug=True)
     
