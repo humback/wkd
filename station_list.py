52743ba5b1948f7a74f9eaac276a13c124b196d0
humback <humback@gmail.com>
2023-04-20 19:59:50 +0200

    station json format change

--- station_list.py
+++ station_list.py
@@ -1,13 +1,12 @@
+import csv
 import json
-
 import io
 
 def stationList():
     with io.open('wkd_gtfs/stops.txt', 'r', encoding='utf8') as f:
-        rows = [line.strip().split(',') for line in f][1:]  # skip first row and split values
-    data = {}
-    for row in rows:
-        key = row.pop(0)
-        data[key] = row
-    json_data = json.dumps(data, ensure_ascii=False)
+        reader = csv.reader(f)
+        headers = next(reader)  # get the column names from the first row
+        data = [dict(zip(headers, row)) for row in reader]
+
+    json_data = json.dumps(data, ensure_ascii=False) # convert list to JSON string
     return json_data
