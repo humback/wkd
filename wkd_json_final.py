be77d7e488ae3ecfb231e02bc39c57cc5a8b028c
humback <humback@gmail.com>
2023-07-20 13:15:19 +0200

    update

--- wkd_json_final.py
+++ wkd_json_final.py
@@ -18,11 +18,14 @@ def scheduleDateTime(stop_time):
     time_obj = datetime.strptime(stop_time, "%H:%M:%S").time()
     dt_with_time = datetime.combine(dt, time_obj)
     dt_with_time = pytz.timezone('Europe/Warsaw').localize(dt_with_time)
+    #print (dt_with_time)
     return dt_with_time
 def get_direction(x):
     
     try:
-        num = int(x.split("D-")[1])
+       
+        #num = int(x.split("D-")[1])
+        num = int(x[2:10])
         #print(num)
         return 'W' if num % 2 == 0 else 'P'
     except (ValueError, IndexError):
@@ -34,16 +37,26 @@ def gtfsRtUpdate(stop_id, direction):
     feed.ParseFromString(response.content)
     #print(response.content)
     json_wkd = MessageToJson(feed)
-    print(json_wkd)
+    #print(json_wkd)
     json_wkd=json.loads(json_wkd)
     #print(json_wkd)
     json_wkd=json_wkd['entity']
-    
+    print (json_wkd)
     df = pd.json_normalize(json_wkd, record_path=['tripUpdate','stopTimeUpdate'],meta=['id'])
+   
     tz = pytz.timezone('Europe/Warsaw')
     df['departure.time'] = pd.to_datetime(df['departure.time'], unit='s').apply(lambda x: pd.Timestamp(x).tz_localize(None).tz_localize('UTC').tz_convert(tz))
     actual = df[df['stopId'] == stop_id]
+    #wyciannie daty z id
+    
+    print("-----------------------------------------------------------------")
+    print (actual)
+    actual["id"] = actual["id"].apply(lambda x: x[11:])
+    print("-----------------------------------------------------------------")
+    print (actual)
+    print (rozklad)
     merged_df = pd.merge(actual, rozklad, left_on=['id','stopId'], right_on=['trip_id','stop_id'], how='left')
+    print (merged_df)
     merged_df = merged_df.drop(columns=['trip_id','stop_id','departure_time'])
     
     merged_df['arrival_time'] = merged_df['arrival_time'].fillna(merged_df['departure.time'].apply(lambda x: x.strftime('%H:%M:%S')))
@@ -53,18 +66,20 @@ def gtfsRtUpdate(stop_id, direction):
     #merged_df['id'] = merged_df['id'].apply(lambda x: x.split(":")[1])
     merged_df['direction'] = merged_df['id'].apply(get_direction)
     merged_df=merged_df[['id','stopId','direction','delay','arrival_time','departure.time']]
-
+    print (merged_df)
     #merged_df['arrival_time'] = merged_df['arrival_time'].apply(lambda x: x.int(datetime_obj.timestamp()))
-
+    
     mask = merged_df['direction'] == direction
     merged_df=merged_df[mask]
-    
+    print (mask)
+    print (merged_df)
     merged_df.rename(columns = {'departure.time':'realDepartureTime','arrival_time':'scheduleDepartureTime'}, inplace = True)
     merged_df.sort_values(by='realDepartureTime', inplace=True)
     
     
     merged_df = merged_df.drop(columns=['direction','stopId'])
-    print (merged_df)
+    
+    print (" *************************")
     merged_df.drop_duplicates()
     merged_dict=merged_df.to_dict(orient="records")
     merged_dict = {"trains":merged_dict}
@@ -77,7 +92,7 @@ def gtfsRtUpdate(stop_id, direction):
 # %%
 if __name__ == "__main__":
     delay_df= gtfsRtUpdate('nwwar','P')
-    print(delay_df)
+    #print(delay_df)
 
 # %%
 
